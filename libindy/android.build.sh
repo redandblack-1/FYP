139edf8f26eaaca427561a40095a70df7cb3ffa0
redandblack-1 <55785921+redandblack-1@users.noreply.github.com>
2021-01-01 16:19:20 +0600

    Add files via upload

--- libindy/android.build.sh
+++ libindy/android.build.sh
@@ -12,10 +12,9 @@ export BOLD=`tput bold`
 export RESET=`tput sgr0`
 
 set -e
-set -o pipefail
-WORKDIR=${PWD}
-LIBINDY_WORKDIR=${WORKDIR}
-CI_DIR="${LIBINDY_WORKDIR}/ci"
+
+WORKDIR="$( cd "$(dirname "$0")" ; pwd -P )"
+CI_DIR="${WORKDIR}/../../libindy/ci"
 export ANDROID_BUILD_FOLDER="/tmp/android_build"
 DOWNLOAD_PREBUILTS="0"
 
@@ -38,106 +37,83 @@ fi
 source ${CI_DIR}/setup.android.env.sh
 
 create_cargo_config(){
-mkdir -p ${LIBINDY_WORKDIR}/.cargo
-cat << EOF > ${LIBINDY_WORKDIR}/.cargo/config
+mkdir -p ${WORKDIR}/.cargo
+cat << EOF > ${WORKDIR}/.cargo/config
 [target.${TRIPLET}]
 ar = "$(realpath ${AR})"
 linker = "$(realpath ${CC})"
 EOF
 }
-
-normalize_dir(){
-    case "$1" in
-    /*) echo "$1";;
-    ~/*) echo "$1";;
-    *) echo "$(pwd)/$1";;
-    esac
-}
-
 setup_dependencies(){
-    if [ "${DOWNLOAD_PREBUILTS}" == "1" ]; then
+   if [ "${DOWNLOAD_PREBUILTS}" == "1" ]; then
         setup_dependencies_env_vars ${ABSOLUTE_ARCH}
         else
             echo "${BLUE}Not downloading prebuilt dependencies. Dependencies locations have to be passed${RESET}"
             if [ -z "${OPENSSL_DIR}" ]; then
-
-                OPENSSL_DIR=$(normalize_dir "openssl_${ABSOLUTE_ARCH}")
-                if [ -d "${OPENSSL_DIR}" ]; then
+                OPENSSL_DIR="openssl_${ABSOLUTE_ARCH}"
+                if [ -d "${OPENSSL_DIR}" ] ; then
                     echo "${GREEN}Found ${OPENSSL_DIR}${RESET}"
-                elif [ -z "$2" ]; then
+                elif [ -z "$3" ]; then
                     echo STDERR "${RED}Missing OPENSSL_DIR argument and environment variable${RESET}"
                     echo STDERR "${BLUE}e.g. set OPENSSL_DIR=<path> for environment or openssl_${ABSOLUTE_ARCH}${RESET}"
                     exit 1
                 else
-                    OPENSSL_DIR=$2
+                    OPENSSL_DIR=$3
                 fi
             fi
 
             if [ -z "${SODIUM_DIR}" ]; then
-                SODIUM_DIR=$(normalize_dir "libsodium_${ABSOLUTE_ARCH}")
+                SODIUM_DIR="libsodium_${ABSOLUTE_ARCH}"
                 if [ -d "${SODIUM_DIR}" ] ; then
                     echo "${GREEN}Found ${SODIUM_DIR}${RESET}"
-                elif [ -z "$3" ]; then
+                elif [ -z "$4" ]; then
                     echo STDERR "${RED}Missing SODIUM_DIR argument and environment variable${RESET}"
                     echo STDERR "${BLUE}e.g. set SODIUM_DIR=<path> for environment or libsodium_${ABSOLUTE_ARCH}${RESET}"
                     exit 1
                 else
-                    SODIUM_DIR=$3
+                    SODIUM_DIR=$4
                 fi
             fi
-
-            if [ -z "${LIBZMQ_DIR}" ] ; then
-                LIBZMQ_DIR=$(normalize_dir  "libzmq_${ABSOLUTE_ARCH}")
-                if [ -d "${LIBZMQ_DIR}" ] ; then
-                    echo "${GREEN}Found ${LIBZMQ_DIR}${RESET}"
-                elif [ -z "$4" ] ; then
-                    echo STDERR "${RED}Missing LIBZMQ_DIR argument and environment variable${RESET}"
-                    echo STDERR "${BLUE}e.g. set LIBZMQ_DIR=<path> for environment or libzmq_${ABSOLUTE_ARCH}${RESET}"
-                    exit 1
-                else
-                    LIBZMQ_DIR=$4
-                fi
-            fi
-
 
     fi
+
+    if [ -z "${INDY_DIR}" ] ; then
+            INDY_DIR="libindy_${ABSOLUTE_ARCH}"
+            if [ -d "${INDY_DIR}" ] ; then
+                echo "${GREEN}Found ${INDY_DIR}${RESET}"
+            elif [ -z "$2" ] ; then
+                echo STDERR "${RED}Missing INDY_DIR argument and environment variable${RESET}"
+                echo STDERR "${BLUE}e.g. set INDY_DIR=<path> for environment or libindy_${ABSOLUTE_ARCH}${RESET}"
+                exit 1
+            else
+                INDY_DIR=$2
+            fi
+
+        if [ -d "${INDY_DIR}/lib" ] ; then
+            INDY_DIR="${INDY_DIR}/lib"
+        fi
+     fi
+
+
 }
 
-statically_link_dependencies_with_libindy(){
-    echo "${BLUE}Statically linking libraries togather${RESET}"
-    echo "${BLUE}Output will be available at ${ANDROID_BUILD_FOLDER}/libindy_${ABSOLUTE_ARCH}/lib/libindy.so${RESET}"
-    $CC -v -shared -o${ANDROID_BUILD_FOLDER}/libindy_${ABSOLUTE_ARCH}/lib/libindy.so -Wl,--whole-archive \
-        ${WORKDIR}/target/${TRIPLET}/release/libindy.a \
-        ${TOOLCHAIN_DIR}/sysroot/usr/lib/${ANDROID_TRIPLET}/libm.a \
-        ${OPENSSL_DIR}/lib/libssl.a \
-        ${OPENSSL_DIR}/lib/libcrypto.a \
-        ${SODIUM_LIB_DIR}/libsodium.a \
-        ${LIBZMQ_LIB_DIR}/libzmq.a \
-        -Wl,--no-whole-archive -z muldefs -L${TOOLCHAIN_DIR}/sysroot/usr/lib/${ANDROID_TRIPLET}/${TARGET_API} -lz -llog -lc++_shared
-}
 
 package_library(){
-
-   export PACKAGE_DIR=${ANDROID_BUILD_FOLDER}/libindy_${ABSOLUTE_ARCH}
-
+    echo "${GREEN}Packaging in zip file${RESET}"
+    PACKAGE_DIR=${ANDROID_BUILD_FOLDER}/libvcx_${ABSOLUTE_ARCH}
+    mkdir -p ${PACKAGE_DIR}/include
     mkdir -p ${PACKAGE_DIR}/lib
 
-    cp -rf "${WORKDIR}/include" ${PACKAGE_DIR}
-    cp "${WORKDIR}/target/${TRIPLET}/release/libindy.a" ${PACKAGE_DIR}/lib
-    cp "${WORKDIR}/target/${TRIPLET}/release/libindy.so" ${PACKAGE_DIR}/lib
-    if [ "${TARGET_ARCH}" != "x86_64" ]; then
-        mv "${PACKAGE_DIR}/lib/libindy.so" "${PACKAGE_DIR}/lib/libindy_shared.so" &&
-        statically_link_dependencies_with_libindy
-    fi
-    pushd ${LIBINDY_WORKDIR}
-        rm -f libindy_android_${ABSOLUTE_ARCH}.zip
+    cp "${WORKDIR}/target/${TRIPLET}/release/libvcx.a" ${PACKAGE_DIR}/lib
+    cp "${WORKDIR}/target/${TRIPLET}/release/libvcx.so" ${PACKAGE_DIR}/lib
+
+     pushd ${WORKDIR}
+        rm -f libvcx_android_${ABSOLUTE_ARCH}.zip
         cp -rf ${PACKAGE_DIR} .
-        if [ -z "${LIBINDY_VERSION}" ]; then
-            zip -r libindy_android_${ABSOLUTE_ARCH}.zip libindy_${ABSOLUTE_ARCH} &&
-            echo "${BLUE}Zip file available at ${PWD}/libindy_android_${ABSOLUTE_ARCH}.zip ${RESET}"
+        if [ -z "${LIBVCX_VERSION}" ]; then
+            zip -r libvcx_android_${ABSOLUTE_ARCH}.zip libvcx_${ABSOLUTE_ARCH}
         else
-            zip -r libindy_android_${ABSOLUTE_ARCH}_${LIBINDY_VERSION}.zip libindy_${ABSOLUTE_ARCH} &&
-            echo "${BLUE}Zip file available at ${PWD}/libindy_android_${ABSOLUTE_ARCH}_${LIBINDY_VERSION}.zip ${RESET}"
+            zip -r libvcx_android_${ABSOLUTE_ARCH}_${LIBVCX_VERSION}.zip libvcx_${ABSOLUTE_ARCH}
         fi
 
     popd
@@ -147,17 +123,14 @@ build(){
     echo "**************************************************"
     echo "Building for architecture ${BOLD}${YELLOW}${ABSOLUTE_ARCH}${RESET}"
     echo "Toolchain path ${BOLD}${YELLOW}${TOOLCHAIN_DIR}${RESET}"
-    echo "ZMQ path ${BOLD}${YELLOW}${LIBZMQ_DIR}${RESET}"
     echo "Sodium path ${BOLD}${YELLOW}${SODIUM_DIR}${RESET}"
-    echo "Openssl path ${BOLD}${YELLOW}${OPENSSL_DIR}${RESET}"
-    echo "Artifacts will be in ${YELLOW}${GREEN}${ANDROID_BUILD_FOLDER}/libindy_${ABSOLUTE_ARCH}${RESET}"
+    echo "Indy path ${BOLD}${YELLOW}${INDY_DIR}${RESET}"
+    echo "Artifacts will be in ${BOLD}${YELLOW}${ANDROID_BUILD_FOLDER}/libvcx_${ABSOLUTE_ARCH}${RESET}"
     echo "**************************************************"
     pushd ${WORKDIR}
         rm -rf target/${TRIPLET}
         cargo clean
-        RUSTFLAGS="-C link-args=-Wl -lc++_shared" \
         cargo build --release --target=${TRIPLET}
-
     popd
 }
 
@@ -167,4 +140,5 @@ setup_dependencies
 set_env_vars
 create_standalone_toolchain_and_rust_target
 create_cargo_config
-build && package_library
+build
+package_library
